288c8577dbbe578fa7c437ff3852e4f70d48b7ea
Maxime Leclercq <m.leclercq@monsieurbiz.com>
2025-05-20 09:46:35 +0200

    feat: migrate to symfony workflows

---
+++ src/EventListener/Workflow/BlogArticleSavePublishDateListener.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Blog plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusBlogPlugin\EventListener\Workflow;
+
+use MonsieurBiz\SyliusBlogPlugin\Entity\ArticleInterface;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\Workflow\Event\CompletedEvent;
+use Webmozart\Assert\Assert;
+
+#[AsEventListener(
+    event: 'workflow.monsieurbiz_blog_article.completed.published'
+)]
+final class BlogArticleSavePublishDateListener
+{
+    public function __invoke(CompletedEvent $event): void
+    {
+        $article = $event->getSubject();
+        Assert::isInstanceOf($article, ArticleInterface::class);
+
+        $article->publish();
+    }
+}
